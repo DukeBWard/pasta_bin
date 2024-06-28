f4041d2d2fb9c3406129a4a8f5f98035f09c978c
Luke Ward <lbw4459@rit.edu>
2024-06-28 09:43:38 -0400

    added some smol things

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,1 +1,52 @@
 package backend
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// Struct to hold form data
+type FormData struct {
+	UserInput string
+}
+
+// Handler to serve the HTML form
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+// Handler to process form submission
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse form data
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Could not parse form", http.StatusBadRequest)
+		return
+	}
+
+	// Get user input
+	userInput := r.FormValue("userInput")
+
+	// Process the user input (for demonstration, we'll just print it)
+	fmt.Fprintf(w, "You entered: %s\n", userInput)
+}
+
+func main() {
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/submit", submitHandler)
+
+	fmt.Println("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
