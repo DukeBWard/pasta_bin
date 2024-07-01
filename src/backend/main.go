a11e6a1b065569c0716b0d1bada28c1417f7d1f4
Luke Ward <wardlukeb@gmail.com>
2024-07-01 19:08:56 -0400

    figuring out what db i wanna use for url creation

--- src/backend/main.go
+++ src/backend/main.go
@@ -7,12 +7,10 @@ import (
 	"net/http"
 )
 
-// Struct to hold form data
 type FormData struct {
 	UserInput string
 }
 
-// Handler to serve the HTML form
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("../view/index.html", "../view/style.css")
 	if err != nil {
@@ -23,24 +21,20 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-// Handler to process form submission
 func submitHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// Parse form data
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Could not parse form", http.StatusBadRequest)
 		return
 	}
 
-	// Get user input
-	userInput := r.FormValue("userInput")
+	userInput := r.FormValue("userInputHidden")
 
-	// Process the user input (for demonstration, we'll just print it)
 	fmt.Fprintf(w, "You entered: %s\n", userInput)
 }
 
